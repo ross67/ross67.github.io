093d4e1dde7e830bd0bf37f31282abe34897e893
Ross Jiang <zj522@nyu.edu>
2018-03-25 18:03:21 +0400

    added click

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,2 +1,15 @@
-//something
+let img=document.getElementById('img')
+// let img2=document.getElementById('img2')
+// let images = [im1,img2]
 
+
+    function changeImg()
+    {
+        if (img.getAttribute('src') == "dumb.jpg")
+        {
+            img.src = "samuel.jpg";
+        }else if(img.getAttribute('src') == "samuel.jpg"){
+          img.src ="doge.jpg";
+        }
+
+    }
