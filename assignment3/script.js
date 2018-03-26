1620f316785c53c749d98f93b98f23d27db0b56e
Ross Jiang <zj522@nyu.edu>
2018-03-27 01:28:58 +0400

    sound files

--- assignment3/script.js
+++ assignment3/script.js
@@ -3,18 +3,44 @@ let img=document.getElementById('img')
 // let images = [im1,img2]
 
 
-    function changeImg()
+    function changeImgForward()
     {
         if (img.getAttribute('src') == "panel1.jpg")
         {
             img.src = "panel2.jpg";
+            $("header").addClass("hide");
         }else if(img.getAttribute('src') == "panel2.jpg"){
           img.src ="panel4.jpg";
+          $("body").addClass("dead");
+          $("#img").addClass("transparent");
+
         }else if(img.getAttribute('src') == "panel4.jpg"){
+          img.src ="panel5.jpg";
+          $("body").removeClass("dead");
+          $("#img").removeClass("transparent");
+        }else if(img.getAttribute('src') == "panel5.jpg"){
           img.src ="panel6.jpg";
         }
-
     }
+
+    function changeImgBackward()
+    {
+        if (img.getAttribute('src') == "panel6.jpg")
+        {
+            img.src = "panel5.jpg";
+        }else if(img.getAttribute('src') == "panel5.jpg"){
+          img.src ="panel4.jpg";
+          $("body").addClass("dead");
+        }else if(img.getAttribute('src') == "panel4.jpg"){
+          img.src ="panel2.jpg";
+        }else if(img.getAttribute('src') == "panel2.jpg"){
+
+          $("header").removeClass("hide");
+          img.src ="panel1.jpg";
+
+        }
+    }
+
 
 
     function hover(element) {
