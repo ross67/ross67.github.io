dd3f8377c2f3c0a904b482df12bbb6648742e8e8
ross67 <35699432+ross67@users.noreply.github.com>
2018-04-26 13:00:02 +0400

    Update script.js

--- fun website/script.js
+++ fun website/script.js
@@ -5,8 +5,8 @@ var el = document.querySelector('.js-fade');
 
 document.getElementById("main").addEventListener("click", function(){
 
-  el.classList.remove('is-paused');
+  el.classList.remove('is-paused')
 
-}
+});
 
 // http://www.chrisbuttery.com/articles/fade-in-fade-out-with-javascript/
